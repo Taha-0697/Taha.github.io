1712a81c030cbe34fa84683ed0bad7f5f4b97f53
Taha-0697 <chgatai5.tc@gmail.com>
2021-02-18 21:45:00 +0500

    php

--- assets/php/form.php
+++ assets/php/form.php
@@ -1,14 +1,14 @@
 <?php
 
 
-if($_POST["message"]) {
-
+//if($_POST["message"]) {
 
 mail("chaghtai5.tc@gmail.com", "PORTFOLIO",
 
 
 //$_POST["insert your message here"]. "From: an@email.address");
 echo "Name : ".$_POST[name];
+
 
 }
 
