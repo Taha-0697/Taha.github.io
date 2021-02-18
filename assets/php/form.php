984b8a8d634b5c74e8d5abacce2af79eb812401a
Taha-0697 <chgatai5.tc@gmail.com>
2021-02-18 21:31:05 +0500

    php

--- assets/php/form.php
+++ assets/php/form.php
@@ -4,11 +4,11 @@
 if($_POST["message"]) {
 
 
-mail("your@email.address", "Here is the subject line",
+mail("chaghtai5.tc@gmail.com", "PORTFOLIO",
 
 
-$_POST["insert your message here"]. "From: an@email.address");
-
+//$_POST["insert your message here"]. "From: an@email.address");
+echo "Name : ".$_POST[name];
 
 }
 
